c0996b40db175cae8aec155f0de5ad621c841ed8
hahayz77 <danilo__danilo@hotmail.com>
2023-06-06 10:12:51 -0300

    new logo 2

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,7 +24,7 @@ const Nav = () => {
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href={"/"} className="flex gap-2 flex-center">
                 <Image src={"/images/logo.svg"} width={30} height={30} className="object-contain" alt="logo" />
-                <p className="logo_text">Promptyou</p>
+                <p className="logo_text">PromptYo</p>
             </Link>
 
             {/* Desktop Navigation */}
