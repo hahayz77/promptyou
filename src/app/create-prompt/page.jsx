20440e5e3d3f1ff4b5aac626db6368a0618efceb
hahayz77 <danilo__danilo@hotmail.com>
2023-06-07 07:11:16 -0300

    new prompt and Form submit ok

--- src/app/create-prompt/page.jsx
+++ src/app/create-prompt/page.jsx
@@ -1,17 +1,46 @@
-// "use client"
+"use client"
 
-// import Form from "@/components/Form"
+import Form from "@/components/Form"
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-// const CreatePrompt = () => {
+const CreatePrompt = () => {
 
-//     const [submitting, setSubmitting] = useState(false);
-//     const [post, setPost] = useState({ prompt: "", tag: "" });
+    const router = useRouter();
+    const { data: session } = useSession();
 
-//     return (
-//         <>
-//             <Form />
-//         </>
-//     )
-// }
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({ prompt: "", tag: "" });
 
-// export default CreatePrompt
+    const createPrompt = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+
+        try {
+            const response = await fetch("/api/prompt/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+
+            if (response.ok) router.push("/")
+
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
+    return (
+        <>
+            <Form type='Create' post={post} setPost={setPost} submitting={submitting} handleSubmit={createPrompt} />
+        </>
+    )
+}
+
+export default CreatePrompt
