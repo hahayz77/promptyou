cabbd266c9050ac71a4cb1d420e7d79c280495ba
hahayz77 <danilo__danilo@hotmail.com>
2023-06-13 10:30:54 -0300

    routes update delete ...

---
+++ src/app/api/prompt/route.js
@@ -0,0 +1,14 @@
+import Prompt from "@/models/prompt";
+import { connectToDB } from "@/utils/database";
+
+export const GET = async (request) => {
+    try {
+        await connectToDB()
+
+        const prompts = await Prompt.find({}).populate('creator')
+
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (err) {
+        return new Response("Failed to fetch", { status: 500 })
+    }
+}
